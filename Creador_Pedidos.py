6df684477b115fe49f07502dd590f326e65f47a2
joserozasa <joserozasa@gmail.com>
2020-09-21 18:00:38 -0300

    Se corrigió la funcion total_partes de la clase pedidos, que entrega el total de partes que contiene un pedido

--- Creador_Pedidos.py
+++ Creador_Pedidos.py
@@ -10,9 +10,11 @@ def main():
 
 
 class Pedido():
-    def __init__(self, detalle_pedido, lista_productos):
+    def __init__(self,nombre, detalle_pedido, lista_productos, partes_posibles):
+        self.nombre = nombre
         self.pedido = detalle_pedido
         self.lista_productos = lista_productos
+        self. partes_posibles = partes_posibles
 
 
     def costo_pedido(self):
@@ -23,18 +25,21 @@ class Pedido():
         return costo_total_pedido
 
     def total_partes(self):
+
         total_partes = {}
-        temp_partes = {}
         for product in self.lista_productos:
             if product.nombre in self.pedido:
+                temp_partes = {}
                 for parte in product.q_partes:
-                    temp_partes[parte] = product.q_partes[parte]
-                    z = dict(Counter(temp_partes) + Counter(total_partes))
-                    #print(product.q_partes[parte])
-                    #print(parte)
-                #print(product.q_partes)
-                #print(self.pedido[product.nombre])
-        print(z)
+                    temp_partes[parte] = product.q_partes[parte] * self.pedido[product.nombre]
+                total_partes = Counter(total_partes)
+                temp_partes = Counter(temp_partes)
+                total_partes = total_partes + temp_partes
+        total_partes_dict = dict(total_partes)
+        for parte in self.partes_posibles: # Para rellenar casos donde en que producto no tiene una parte en especial
+            if parte not in total_partes_dict:
+                total_partes_dict[parte] = 0
+        return total_partes_dict
 
 
 if __name__ == '__main__':
--- Main.py
+++ Main.py
@@ -1,32 +1,34 @@
-
 from Creador_Productos import Producto
 from Creador_Pedidos import Pedido
 
+pedido = {"Producto_A": 0, "Producto_B": 10}
 
-pedido = {"Producto_A": 100, "Producto_C": 3}
+partes_posibles = {"parte_1": 0, "parte_2": 0, "parte_3": 0, "parte_4": 0, "parte_5": 0}
 
-precio_partes = {"parte_1": 2, "parte_2": 3, "parte_3": 4, "parte_4": 5, "parte_5": 100 }
+precio_partes = {"parte_1": 2, "parte_2": 3, "parte_3": 4, "parte_4": 5, "parte_5": 100}
 
 listado_generador_productos = {
-        "Producto_A" : {"parte_1": 1, "parte_2": 1, "parte_3": 1, "parte_4": 1, "parte_5": 1},
-        "Producto_B" : {"parte_1": 1, "parte_3": 3, "parte_4": 1, "parte_5": 1},
-        "Producto_C" : {"parte_1": 4, "parte_2": 4, "parte_3": 3, "parte_4": 5, "parte_5": 1},
-        "Producto_D" : {"parte_1": 1, "parte_2": 1, "parte_3": 1, "parte_4": 1},
-        "Producto_E" : {"parte_1": 1, "parte_2": 1, "parte_3": 1, "parte_4": 1, "parte_5": 100}
-        }
+    "Producto_A": {"parte_1": 1, "parte_2": 1, "parte_3": 1, "parte_4": 1, "parte_5": 1},
+    "Producto_B": {"parte_1": 1, "parte_3": 3, "parte_4": 1, "parte_5": 1},
+    "Producto_C": {"parte_1": 4, "parte_2": 4, "parte_3": 3, "parte_4": 5, "parte_5": 1},
+    "Producto_D": {"parte_1": 1, "parte_2": 1, "parte_3": 1, "parte_4": 1},
+    "Producto_E": {"parte_1": 1, "parte_2": 1, "parte_3": 1, "parte_4": 1, "parte_5": 100}
+}
+
+
 def iniciador_productos(listado_productos):  # Devuelve una lista con los objetos de productos detallados en listado_productos
     lista_clase_productos = []
     for key in listado_productos:
         lista_clase_productos.append(Producto(key, precio_partes, listado_productos[key]))
     return lista_clase_productos
+
+
 lista_de_clases = iniciador_productos(listado_generador_productos)
 
+pedido1 = Pedido('Pedido 1', pedido, lista_de_clases, partes_posibles)
+
+total_de_partes = pedido1.total_partes()
+costo_total_pedido = pedido1.costo_pedido()
 
 
-
-
-pedido1 = Pedido(pedido, lista_de_clases)
-
-dict = pedido1.total_partes()
-
-print(dict)
+print('El pedido {} costó {} y tiene {} Partes 1, {} Partes 2, {} Partes 3, {} Partes 4, {} Partes 5'.format(pedido1.nombre, pedido1.costo_pedido(), pedido1.total_partes()['parte_1'],  pedido1.total_partes()['parte_2'],  pedido1.total_partes()['parte_3'],  pedido1.total_partes()['parte_4'],  pedido1.total_partes()['parte_5']) )
